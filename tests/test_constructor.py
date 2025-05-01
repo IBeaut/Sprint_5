acd2d3de0a9f1473426d7cb6e8b414fa70fe6ca2
Artem Sidorov <kkpemenb@gmail.com>
2025-05-01 09:31:40 +0300

    Рефакторинг: перенёс фикстуру в conftest, обновил локаторы, заменил time.sleep на ожидания

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,7 +1,8 @@
 import pytest
-from locators.locators import MainPageLocators
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
-import time
+from locators.constructor_locators import ConstructorPageLocators
 
 URL_MAIN = "https://stellarburgers.nomoreparties.site/"
 
@@ -9,21 +10,22 @@ def test_constructor_sections(driver):
     base_page = BasePage(driver)
     base_page.open(URL_MAIN)
 
-    # Проверка перехода к "Соусам"
-    sauce_button = driver.find_element_by_xpath("//span[text()='Соусы']")
-    sauce_button.click()
-    assert sauce_button.is_displayed()
+    wait = WebDriverWait(driver, 5)
 
-    time.sleep(1)
+    # Проверка таба "Соусы"
+    sauce_tab = wait.until(EC.element_to_be_clickable(ConstructorPageLocators.SAUCES_TAB))
+    sauce_tab.click()
+    active_sauce = wait.until(EC.visibility_of_element_located(ConstructorPageLocators.ACTIVE_TAB("Соусы")))
+    assert active_sauce.is_displayed()
 
-    # Проверка перехода к "Начинкам"
-    filling_button = driver.find_element_by_xpath("//span[text()='Начинки']")
-    filling_button.click()
-    assert filling_button.is_displayed()
+    # Проверка таба "Начинки"
+    fillings_tab = wait.until(EC.element_to_be_clickable(ConstructorPageLocators.FILLINGS_TAB))
+    fillings_tab.click()
+    active_fillings = wait.until(EC.visibility_of_element_located(ConstructorPageLocators.ACTIVE_TAB("Начинки")))
+    assert active_fillings.is_displayed()
 
-    time.sleep(1)
-
-    # Проверка перехода к "Булкам"
-    bun_button = driver.find_element_by_xpath("//span[text()='Булки']")
-    bun_button.click()
-    assert bun_button.is_displayed()
+    # Проверка таба "Булки"
+    buns_tab = wait.until(EC.element_to_be_clickable(ConstructorPageLocators.BUNS_TAB))
+    buns_tab.click()
+    active_buns = wait.until(EC.visibility_of_element_located(ConstructorPageLocators.ACTIVE_TAB("Булки")))
+    assert active_buns.is_displayed()
--- tests/test_profile.py
+++ tests/test_profile.py
@@ -1,39 +1,20 @@
 import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from locators.locators import MainPageLocators, LoginPageLocators, ProfilePageLocators
 from pages.base_page import BasePage
-from utils.user_generator import generate_email, generate_password
-import time
 
 URL_MAIN = "https://stellarburgers.nomoreparties.site/"
-
-@pytest.fixture
-def logged_in_user(driver):
-    # Регистрируем и логиним пользователя
-    email = generate_email()
-    password = "123456"
-    driver.get("https://stellarburgers.nomoreparties.site/register")
-
-    driver.find_element_by_name('name').send_keys("TestUser")
-    driver.find_element_by_name('email').send_keys(email)
-    driver.find_element_by_name('Пароль').send_keys(password)
-    driver.find_element_by_xpath("//button[text()='Зарегистрироваться']").click()
-    time.sleep(2)
-
-    driver.get(URL_MAIN)
-    driver.find_element(*MainPageLocators.PERSONAL_ACCOUNT_BUTTON).click()
-    driver.find_element(*LoginPageLocators.EMAIL_INPUT).send_keys(email)
-    driver.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(password)
-    driver.find_element(*LoginPageLocators.SUBMIT_BUTTON).click()
-    time.sleep(2)
-
-    return email, password
 
 def test_logout_from_profile(driver, logged_in_user):
     base_page = BasePage(driver)
     base_page.open(URL_MAIN)
 
     base_page.click(MainPageLocators.PERSONAL_ACCOUNT_BUTTON)
-    time.sleep(1)
+
+    WebDriverWait(driver, 5).until(
+        EC.visibility_of_element_located(ProfilePageLocators.EXIT_BUTTON)
+    )
     base_page.click(ProfilePageLocators.EXIT_BUTTON)
 
     assert base_page.is_element_visible(LoginPageLocators.SUBMIT_BUTTON)
