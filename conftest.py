303ecf6dacf9024580a90c7376213912cab09ba7
Artem Sidorov <kkpemenb@gmail.com>
2025-05-02 21:58:36 +0300

    Add .gitignore file

--- conftest.py
+++ conftest.py
@@ -1,29 +1,12 @@
+# fixtures/conftest.py
 import pytest
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from utils.user_generator import generate_email
-from locators.locators import MainPageLocators, LoginPageLocators
 
-@pytest.fixture
-def logged_in_user(driver):
-    email = generate_email()
-    password = "123456"
-    driver.get("https://stellarburgers.nomoreparties.site/register")
-
-    driver.find_element(By.NAME, 'name').send_keys("TestUser")
-    driver.find_element(By.NAME, 'email').send_keys(email)
-    driver.find_element(By.NAME, 'Пароль').send_keys(password)
-    driver.find_element(By.XPATH, "//button[text()='Зарегистрироваться']").click()
-
-    WebDriverWait(driver, 5).until(EC.url_contains("login"))
-
-    driver.get("https://stellarburgers.nomoreparties.site/")
-    driver.find_element(*MainPageLocators.PERSONAL_ACCOUNT_BUTTON).click()
-    driver.find_element(*LoginPageLocators.EMAIL_INPUT).send_keys(email)
-    driver.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(password)
-    driver.find_element(*LoginPageLocators.SUBMIT_BUTTON).click()
-
-    WebDriverWait(driver, 5).until(EC.url_matches("https://stellarburgers.nomoreparties.site/"))
-
-    return email, password
+@pytest.fixture(scope="function")
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
--- locators/constructor_locators.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class ConstructorPageLocators:
-    SAUCES_TAB = (By.XPATH, "//span[text()='Соусы']/parent::div")
-    FILLINGS_TAB = (By.XPATH, "//span[text()='Начинки']/parent::div")
-    BUNS_TAB = (By.XPATH, "//span[text()='Булки']/parent::div")
-
-    ACTIVE_TAB = lambda text: (By.XPATH, f"//div[contains(@class, 'tab_tab_type_current')]/span[text()='{text}']")
